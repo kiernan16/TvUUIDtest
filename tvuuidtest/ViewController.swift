d81255925e406dc4a605e49310cee2c7dc96cd4c
Matt Kiernan <kiernan@mail.usf.edu>
2017-06-26 16:39:43 -0400

    Initial commit

--- tvuuidtest/ViewController.swift
+++ tvuuidtest/ViewController.swift
@@ -7,19 +7,34 @@
 //
 
 import UIKit
+import AdSupport
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var idfvLabel: UILabel!
+    
+    @IBOutlet weak var idfaLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewDidLoad()
+        
+        //****IDFV****//
+             idfvLabel.text = UIDevice.current.identifierForVendor!.uuidString
+        
+        //****IDFA****//
+        idfaLabel.text = ASIdentifierManager.shared().advertisingIdentifier.uuidString
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Dispose of any resources that can be recreated.i
     }
-
-
+    
+    
 }
 
